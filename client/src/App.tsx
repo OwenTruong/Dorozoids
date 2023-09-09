072bd8ed99fd43054070204eb07b6e652aebe233
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-09-08 23:56:25 -0400

    added temporary color theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,31 @@
-import { Container } from "@mui/material";
+import { Container, ThemeProvider, createTheme } from '@mui/material';
+
+const colorTheme = createTheme({
+  palette: {
+    primary: {
+      light: '#30006B',
+      main: '#1C0049',
+      dark: '#0D0026',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#97FBDC',
+      main: '#67F3BF',
+      dark: '#0A8541',
+      contrastText: '#000000',
+    },
+    background: {
+      paper: '#0A8541',
+    },
+  },
+});
 
 function App() {
-  return <Container>Hello Owen and Shob!</Container>;
+  return (
+    <ThemeProvider theme={colorTheme}>
+      <Container></Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
